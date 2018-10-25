ef4231404e834b8c148d585e730ee0f2e554f435
Ryan Jeff Brual <lrjbrual@live.com>
2018-10-25 15:07:26 +0200

    Use random tokens for invitation identifiers

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,5 +1,17 @@
 class Invitation < ApplicationRecord
   belongs_to :account
 
+  before_create :generate_token
+
   validates :email, presence: true
+ 
+  private
+
+  def generate_token
+    self.token = SecureRandom.hex(16)
+  end
+
+  def to_param
+    token
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,6 +49,9 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  #config.action_mailer.default_url_options = { :host => "localhost:3000" }
   # config.session_store :cookie_store, key: 'twist', domain: '.lvh.me'
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 end
